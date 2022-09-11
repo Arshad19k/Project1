8e9a367b27d800b12f7e033f53cee51bc7eec548
Arshad19k <110489108+Arshad19k@users.noreply.github.com>
2022-09-11 22:30:37 +0530

    Updates

---
+++ precache-manifest.075557acdca79bd27ee15ada98a05720.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f0330f3e9576dc16ce6",
+    "url": "/TextUtlis-React/static/css/main.4332e849.chunk.css"
+  },
+  {
+    "revision": "3f0330f3e9576dc16ce6",
+    "url": "/TextUtlis-React/static/js/main.3f0330f3.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/TextUtlis-React/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/TextUtlis-React/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "f707c1967730e08ed867",
+    "url": "/TextUtlis-React/static/js/runtime~main.f707c196.js"
+  },
+  {
+    "revision": "cde6fed2be9eb39653bc757ab699597a",
+    "url": "/TextUtlis-React/index.html"
+  }
+];
